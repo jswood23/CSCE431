13f0c6ef0ba9fc512836ffbed712247607895627
root <root@localhost>
2022-09-08 13:58:26 +0000

    generated external_controller and basic views

---
+++ app/controllers/external_controller.rb
@@ -0,0 +1,16 @@
+class ExternalController < ApplicationController
+  def home
+  end
+
+  def events
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def nav_bar
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'external/home'
+  get 'external/events'
+  get 'external/about'
+  get 'external/contact'
+  get 'external/nav_bar'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
