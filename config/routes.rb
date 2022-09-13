0df57bc073a40fdb71feccc42159794a666175e3
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-09-13 10:01:10 -0500

    shortened paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # root route at external/home
   root "external#home"
 
+  # external page routes
+  get "/home", to: "external#home"
+  get "/about", to: "external#about"
+  get "/events", to: "external#events"
+  get "/contact", to: "external#contact"
+
   # default route
   get ":controller(/:action(:id))"
 
