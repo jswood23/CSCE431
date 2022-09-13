2cc0acd73acbf9c0efe5d9ee1ee72b12bcbcb21c
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-09-13 10:42:17 -0500

    removed default route to address brakeman warning

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,5 @@ Rails.application.routes.draw do
   get "/events", to: "external#events"
   get "/contact", to: "external#contact"
 
-  # default route
-  get ":controller(/:action(:id))"
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
