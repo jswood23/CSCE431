96d119df2d859ca55f5438e8c4425833af26864e
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-09-20 11:41:54 -0500

    generated admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,17 +7,22 @@ Rails.application.routes.draw do
   # root route at external/home
   root 'external#home'
 
-  # internal page routes
-  get '/attend', to: 'internal#attend'
-  get '/members', to: 'internal#members'
-  get '/profile', to: 'internal#profile'
-
   # external page routes
   get '/about', to: 'external#about'
   get '/contact', to: 'external#contact'
   get '/events', to: 'external#events'
   get '/home', to: 'external#home'
   get '/login', to: 'external#login'
+
+  # internal page routes
+  get '/attend', to: 'internal#attend'
+  get '/members', to: 'internal#members'
+  get '/profile', to: 'internal#profile'
+  
+  # admin page routes
+  get '/manage_members', to: 'admin#manage_members'
+  get '/manage_pages', to: 'admin#manage_pages'
+  get '/manage_events', to: 'admin#manage_events'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
