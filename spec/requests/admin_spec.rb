2a6c7912127ef3f7a851934db07d9059cc5a6e36
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-09-20 12:06:46 -0500

    updated admin controller and helper specs

--- spec/requests/admin_spec.rb
+++ spec/requests/admin_spec.rb
@@ -3,21 +3,21 @@ require 'rails_helper'
 RSpec.describe "Admins", type: :request do
   describe "GET /manage_members" do
     it "returns http success" do
-      get "/admin/manage_members"
+      get "/manage_members"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /manage_pages" do
     it "returns http success" do
-      get "/admin/manage_pages"
+      get "/manage_pages"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /manage_events" do
     it "returns http success" do
-      get "/admin/manage_events"
+      get "/manage_events"
       expect(response).to have_http_status(:success)
     end
   end
