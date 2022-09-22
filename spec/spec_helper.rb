e41c09fd6d980154fd590ad74c61caaf8a18bbb7
root <root@localhost>
2022-09-22 19:48:12 +0000

    got rid of devise testing lines

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,11 +18,6 @@ SimpleCov.start
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Devise::Test::ControllerHelpers, type: :view
-  config.include Devise::Test::IntegrationHelpers, type: :feature
-
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
