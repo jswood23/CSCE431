8f9a6d43e4ffa67ee4f2bf3681a2c1232913e2f8
MengtingCao <mengting.cao@tamu.edu>
2022-09-21 19:51:29 -0500

    removed test login page

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -17,17 +17,3 @@ RSpec.describe 'Visiting the home page', type: :feature do
     expect(page).to have_content('Attend an Event')
   end
 end
-
-RSpec.describe 'Test Login', type: :feature do
-  scenario 'Correct Login Page'
-    visit '/home'
-    click_on 'Login'
-    expect(page).to have_content('Log In')
-    expect(page).to have_content('Email')
-    expect(page).to have_content('Password')
-    expect(page).to have_content('Remember Me')
-    expect(page).to have_content('Log in')
-    expect(page).to have_content('Sign up')
-    expect(page).to have_content('Forgot your password?')
-  end
-end
