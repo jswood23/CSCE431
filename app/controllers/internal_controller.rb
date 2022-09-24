c6d8dfdd02120a79b47e392f2aca29ef77a88ff6
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-09-23 19:21:22 -0500

    added user permission checks to internal and admin controllers

--- app/controllers/internal_controller.rb
+++ app/controllers/internal_controller.rb
@@ -1,5 +1,17 @@
 class InternalController < ApplicationController
+  before_action :check_has_access
+
+  def check_has_access
+    if current_user
+      return true
+    end
+    redirect_to '/home'
+    return false
+  end
+
   def attend
+    # current_user.admin = true
+    # current_user.save()
   end
 
   def members
