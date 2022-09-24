60961904e63c5bb5b0d6ede91860355b655a04b4
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-09-24 10:11:21 -0500

    is_admin function can check any user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   helper_method :is_admin
 
-  def is_admin
-    if current_user
-      if current_user.admin
+  def is_admin(user = current_user)
+    if user
+      if user.admin
         return true
       end
     end
