75c53a6b036083951991cc355f8e967b0ea44332
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-09-24 12:25:05 -0500

    added get_user_status helper function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :is_admin
+  helper_method :is_admin, :get_user_status
 
   def is_admin(user = current_user)
     if user
@@ -8,5 +8,15 @@ class ApplicationController < ActionController::Base
       end
     end
     return false
+  end
+
+  def get_user_status(user = current_user)
+    if user
+      if user.admin
+        return "Admin"
+      end
+      return "User (no permissions)"
+    end
+    return "Does not exist"
   end
 end
