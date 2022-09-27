1d58875fdc749e6154e33dbab8a1b7d0d9149f1c
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-09-27 11:28:15 -0500

    added member checks to nav bar and member pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-  helper_method :is_admin, :get_user_status
+  helper_method :is_admin, :is_member, :get_user_status
 
   def is_admin(user = current_user)
     if user
       if current_user.id == 1 && !current_user.admin
         current_user.admin = true
+        current_user.member = true
         current_user.save()
       end
       if user.admin
@@ -14,12 +15,22 @@ class ApplicationController < ActionController::Base
     return false
   end
 
+  def is_member(user = current_user)
+    if user
+      return true
+    end
+    return false
+  end
+
   def get_user_status(user = current_user)
     if user
       if user.admin
         return "Admin"
+      elsif user.member
+        return "Member"
+      else
+        return "User (no permissions)"
       end
-      return "User (no permissions)"
     end
     return "Does not exist"
   end
