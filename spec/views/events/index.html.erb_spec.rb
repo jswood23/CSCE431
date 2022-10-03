511333519fd9f0bde830fdddf73991e4c65415c5
Yuning Zhang <yuning.zhang09@gmail.com>
2022-10-03 11:26:50 -0500

    added integration tests for event page

--- spec/views/events/index.html.erb_spec.rb
+++ spec/views/events/index.html.erb_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "events/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "Visiting events page", type: :feature do
+  scenario 'events page has full content' do
+    visit events_path
+    # event attributes
+    expect(page).to have_content('Event Name')
+    expect(page).to have_content('Description')
+    expect(page).to have_content('Date')
+    expect(page).to have_content('Points')
+
+    expect(page).not_to have_content('Create New Event')
+    expect(page).not_to have_content('Passcode')
+    
+  end
 end
