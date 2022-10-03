19218b0e46b557d35e25d7be220e34db029b50ed
Yuning Zhang <yuning.zhang09@gmail.com>
2022-10-03 11:03:03 -0500

    added unit tests for Event

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  subject do 
+    described_class.new(event_name: 'Meeting', description: 'Meeting for organization', date:'2022-10-01', 
+                        points: 1, passcode: '123abc')
+  end
+
+  it 'is valid with all valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.event_name = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a description' do
+    subject.description = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a date' do
+    subject.date = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without points' do
+    subject.points = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without passcode' do
+    subject.passcode = nil
+    expect(subject).not_to be_valid
+  end
 end
