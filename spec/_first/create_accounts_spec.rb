c5d6484a1eea3fd7e4991b804ab72e031cf4839d
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-10-06 14:50:18 -0500

    fixes for creating users

--- spec/_first/create_accounts_spec.rb
+++ spec/_first/create_accounts_spec.rb
@@ -2,15 +2,17 @@ require 'rails_helper'
 require './spec/support/user_helpers'
 
 RSpec.describe('Creating accounts', type: :feature) do
+  # rubocop:disable RSpec/BeforeAfterAll
   before(:all) do
-    ActiveRecord::Base.connection.execute("TRUNCATE users RESTART IDENTITY;")
-    create_accounts()
+    ActiveRecord::Base.connection.execute('TRUNCATE users RESTART IDENTITY;')
+    create_accounts
   end
+  # rubocop:enable RSpec/BeforeAfterAll
 
   it 'asserts that both admin account and member account exists' do
-    log_in_admin()
-    log_out()
-    log_in_member()
-    log_out()
+    log_in_admin
+    log_out
+    log_in_member
+    log_out
   end
 end
--- spec/views/admin/manage_members.html.erb_spec.rb
+++ spec/views/admin/manage_members.html.erb_spec.rb
@@ -3,21 +3,20 @@ require './spec/support/user_helpers'
 
 RSpec.describe('admin/manage_members.html.erb', type: :feature) do
   it 'shows header and subheaders' do
-    log_in_admin()
+    log_in_admin
     visit 'manage_members'
     expect(page).to(have_content('Manage Members'))
     expect(page).to(have_content('New Users'))
     expect(page).to(have_content('Members'))
-    log_out()
+    log_out
   end
 
   it 'turns new user into member' do
-    log_in_admin()
+    log_in_admin
     visit 'manage_members'
 
     # new user shows up
     expect(page).not_to have_content('No new users.')
-
 
     # turn new user into member
     member_email = UserHelpers.class_variable_get(:@@member_email)
@@ -71,8 +70,13 @@ RSpec.describe('admin/manage_members.html.erb', type: :feature) do
 =======
     expect(page).to(have_content('No new users.'))
     assert User.all.length == 1
+<<<<<<< HEAD
     
     log_out()
 >>>>>>> rspec fixes
+=======
+
+    log_out
+>>>>>>> fixes for creating users
   end
 end
