9da6352f28673a168344d3116fa94be5ec40fc38
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-10-10 01:20:30 -0500

    created information table

---
+++ app/models/information.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Information < ApplicationRecord
+  belongs_to :user, optional: true
+
+  after_create do |info|
+    info.points = 0
+    info.position = "Member"
+    info.bios = ""
+    info.phone = ""
+    info.private = false
+  end
+end
