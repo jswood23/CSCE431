86cce7ca42b196c567f0cb9eceeb4037307c1374
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-10-10 11:11:39 -0500

    rubocop fixes on rebase

--- spec/feature/google_auth_spec.rb
+++ spec/feature/google_auth_spec.rb
@@ -1,12 +1,14 @@
-require "rails_helper"
+# frozen_string_literal: true
 
-feature "google_auth" do
-    scenario "login" do
-        visit new_user_session_path
-        find("#google_signin_button").click
-        expect(page).to have_content('Successfully authenticated from Google account.')
-        visit edit_user_registration_path
-        click_on 'Update'
-        expect(page).to have_content('Your account has been updated successfully.')
-    end
+require 'rails_helper'
+
+RSpec.describe('Creating an account with google auth', type: :feature) do
+  it 'login' do
+    visit new_user_session_path
+    find('#google_signin_button').click
+    expect(page).to(have_content('Successfully authenticated from Google account.'))
+    visit edit_user_registration_path
+    click_on 'Update'
+    expect(page).to(have_content('Your account has been updated successfully.'))
+  end
 end
