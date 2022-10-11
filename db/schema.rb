75d7152a34b6475f5b99186bb121954feffd9a8e
root <root@localhost>
2022-10-11 01:06:29 +0000

    added events to schema and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2022_10_10_033744) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +21,16 @@ ActiveRecord::Schema.define(version: 2022_10_10_033744) do
     t.string "phone"
     t.integer "points"
     t.boolean "private"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "event_name"
+    t.text "description"
+    t.string "passcode"
+    t.datetime "date"
+    t.integer "points"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
