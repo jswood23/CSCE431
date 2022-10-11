264f0b12c16981d50cac3e3a6f38e674abcd2e14
MengtingCao <mengting.cao@tamu.edu>
2022-10-11 01:20:48 -0500

    Moved event table model tests to model folder

--- spec/feature/events_test_spec.rb
+++ spec/feature/events_test_spec.rb
@@ -177,35 +177,3 @@ end
 #        log_out
 #    end
 #end
-
-RSpec.describe Event, type: :model do
-    subject do
-        described_class.new(event_name: 'Test Event', description: 'This is a test event', 
-            passcode: 'test', date: Time.now, points: 1)
-    end
-
-    it 'is valid with valid attributes' do
-        expect(subject).to be_valid
-    end
-
-    it 'is not valid without a name' do
-        subject.event_name = nil
-        expect(subject).not_to be_valid
-    end
-    it 'is not valid without a description' do
-        subject.description = nil
-        expect(subject).not_to be_valid
-    end
-    it 'is not valid without a passcode' do
-        subject.passcode = nil
-        expect(subject).not_to be_valid
-    end
-    it 'is not valid without a date' do
-        subject.date = nil
-        expect(subject).not_to be_valid
-    end
-    it 'is not valid without a points' do
-        subject.points = nil
-        expect(subject).not_to be_valid
-    end
-end
