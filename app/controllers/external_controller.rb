8eb86e71a7be8862aac58c1c355c97b9d7b14ce2
root <root@localhost>
2022-10-11 01:04:13 +0000

    Added sorted events list call to external and internal event controllers

--- app/controllers/external_controller.rb
+++ app/controllers/external_controller.rb
@@ -5,7 +5,12 @@ class ExternalController < ApplicationController
 
   def contact; end
 
-  def events; end
+  def events
+    #order events by date
+    @events = Event.order('date ASC')
+    @upcoming_events = Event.order('date ASC').where("date > ?", Time.now)
+    @past_events = Event.order('date DESC').where("date < ?", Time.now)
+  end
 
   def home; end
 end
--- app/controllers/internal_controller.rb
+++ app/controllers/internal_controller.rb
@@ -3,7 +3,12 @@
 class InternalController < ApplicationController
   before_action :check_has_member_access
 
-  def attend; end
+  def attend
+    #order events by date
+    @events = Event.order('date ASC')
+    @upcoming_events = Event.order('date ASC').where("date > ?", Time.now)
+    @past_events = Event.order('date DESC').where("date < ?", Time.now)
+  end
 
   def members; end
 
