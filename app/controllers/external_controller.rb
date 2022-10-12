591ece56a24ae63ed7abc5416158ea840ec5dfbb
Nicholas Roth <nroth020@gmail.com>
2022-10-12 15:31:58 -0500

    <a> to link_to

--- app/controllers/external_controller.rb
+++ app/controllers/external_controller.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 class ExternalController < ApplicationController
-  def about; end
+  def about
+    #@page = Page.where(page_name: 'about').order("created_at DESC").first
+  end
 
-  def contact; end
+  def contact
+    #@page = Page.where(page_name: 'contact').order("created_at DESC").first
+    @hrefs = { email: "mailto:hannahbartosh@tamu.edu", facebook: "https://www.facebook.com/MCHSOaggies/" }
+  end
 
-  def events; end
+  def events
+    #@page = Page.where(page_name: 'events').order("created_at DESC").first
+  end
 
-  def home; end
+  def home
+    #@page = Page.where(page_name: 'home').order("created_at DESC").first
+  end
 end
