3493494d4e63eb15ff977fbcbef4ba0401c801c1
MengtingCao <mengting.cao@tamu.edu>
2022-10-13 21:27:30 -0500

    Added information table attributes to update user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -59,7 +59,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name])
+    devise_parameter_sanitizer.permit(
+      :account_update, 
+      keys: [:full_name, 
+        information_attributes: [:position, :phone, :bios, :private]
+      ]
+    )
   end
 
   # The path used after sign up.
@@ -74,4 +79,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,21 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:google_oauth2]
   has_one :information, dependent: :destroy
 
+  accepts_nested_attributes_for :information, allow_destroy: true
+
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.full_name = auth.info.name
-      user.avatar_url = auth.info.image
+    if self.where(email: auth.info.email).exists?
+      return_user = self.where(email: auth.info.email).first
+      return_user.provider = auth.provider
+      return_user.uid = auth.uid
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0, 20]
+        user.full_name = auth.info.name
+        user.avatar_url = auth.info.image
+      end
     end
+    return_user
   end
 end
