99be6ace638561211c78c8ece0b473d0589da1ea
MengtingCao <mengting.cao@tamu.edu>
2022-10-14 22:52:59 -0500

    add oauth image to devise account

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,9 @@ class User < ApplicationRecord
       return_user = where(email: auth.info.email).first
       return_user.provider = auth.provider
       return_user.uid = auth.uid
+      return_user.avatar_url = auth.info.image
+      return_user.save!
+      return_user
     else
       where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
         user.email = auth.info.email
