1e2fb69368fca7e90fb682cd5120701f22ff2882
MengtingCao <mengting.cao@tamu.edu>
2022-10-14 22:51:08 -0500

    removed unused variables

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,6 @@ class EventsController < ApplicationController
   # GET /events or /events.json
   def index
     # order events by date
-    @events = Event.order('date ASC')
     @events_today = Event.where(date: Time.zone.today.all_day)
     @upcoming_events = Event.order('date ASC').where('date > ?', Time.zone.now.end_of_day)
     @past_events = Event.order('date DESC').where('date < ?', Time.zone.now.beginning_of_day)
