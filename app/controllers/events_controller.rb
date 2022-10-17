4520d16bdf7d615b109d768045a9f4974b303b2f
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-10-17 00:28:21 -0500

    new permissions check for attend_event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,8 @@
 
 class EventsController < ApplicationController
   before_action :set_event, only: %i[show edit update destroy]
-  before_action :check_has_access
+  before_action :check_has_admin_access, only: %i[index show new edit create update destroy]
+  before_action :check_has_member_access, only: %i[attend_event]
 
   # GET /events or /events.json
   def index
@@ -84,8 +85,16 @@ class EventsController < ApplicationController
     params.require(:event).permit(:event_name, :description, :passcode, :date, :points)
   end
 
-  def check_has_access
+  def check_has_admin_access
     return true if admin?
+
+    flash.alert = 'You do not have permission to access this.'
+    redirect_to(home_path)
+    false
+  end
+
+  def check_has_member_access
+    return true if member?
 
     flash.alert = 'You do not have permission to access this.'
     redirect_to(home_path)
