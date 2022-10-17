e6b71703177cccef86e9e1116a551fa14f7ce57d
julianaLeano <julexd125@gmail.com>
2022-10-17 18:56:31 -0500

    rubocop changes

--- spec/views/external/home.html.erb_spec.rb
+++ spec/views/external/home.html.erb_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe('external/home.html.erb', type: :feature) do
   before do
     visit '/home'
   end
+
   it 'shows introduction content' do
     expect(page).to(have_content('Welcome to the Maternal and Child Health Student Organization'))
   end
