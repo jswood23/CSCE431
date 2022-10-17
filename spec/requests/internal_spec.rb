06816b5b330aa3131b46edecf687d1ec83873293
julianaLeano <julexd125@gmail.com>
2022-10-17 16:17:41 -0500

    merge conflicts

--- spec/requests/internal_spec.rb
+++ spec/requests/internal_spec.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Internals", type: :request do
-  describe "GET /contact" do
-    it "returns http success" do
-      get "/internal/contact"
-      expect(response).to have_http_status(:success)
+RSpec.describe('Internals', type: :request) do
+  describe 'GET /contact' do
+    it 'returns http success' do
+      get '/internal/contact'
+      expect(response).to(have_http_status(:success))
     end
   end
-
 end
