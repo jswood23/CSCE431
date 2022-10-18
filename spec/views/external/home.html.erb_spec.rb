2c9b2e34e5bf594ddfcf304d9024c8a5840b42d4
julianaLeano <julexd125@gmail.com>
2022-10-17 23:07:08 -0500

    minor changes

--- spec/views/external/home.html.erb_spec.rb
+++ spec/views/external/home.html.erb_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-RSpec.describe('external/home.html.erb', type: :view) do
+RSpec.describe('external/home.html.erb', type: :feature) do
   it 'shows introduction content' do
     visit '/home'
     expect(page).to(have_content('Welcome to the Maternal and Child Health Student Organization'))
