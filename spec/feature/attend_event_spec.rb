df5da5d44fcc75aadcd7e759ea406ed40103e781
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-10-20 13:42:48 -0500

    test removes user from event attendance

--- spec/feature/attend_event_spec.rb
+++ spec/feature/attend_event_spec.rb
@@ -17,15 +17,19 @@ RSpec.describe('Event Attendance', type: :feature) do
       log_out
     end
 
-    it 'user attends event' do
+    it 'user attends event and admin removes user from attendance' do
       log_in_admin
       visit '/attend'
+
+      # attend event
       this_event_card = find(:css, '#event_1')
       this_event_passcode = UserHelpers.class_variable_get(:@@event_passcode)
       within(this_event_card) do
         fill_in 'Password', with: this_event_passcode
         click_on 'I\'m here!'
       end
+
+      # check that user has attended event
       expect(AttendanceRecord.where(event_id: 1).count).to(eq(1))
       visit events_path
       this_event_card = find(:css, '#event_1')
@@ -37,7 +41,14 @@ RSpec.describe('Event Attendance', type: :feature) do
       within(this_row) do
         expect(page).not_to(have_content('No one has attended this event yet.'))
         expect(page).to(have_content(admin_name))
+        expect(AttendanceRecord.where(event_id: 1).count).to(eq(1))
+
+        # remove user from event
+        click_on 'Remove'
       end
+      expect(page).to(have_content("Member #{admin_name} has been successfully removed from event."))
+      expect(page).to(have_content('No one has attended this event yet.'))
+      expect(AttendanceRecord.where(event_id: 1).count).to(eq(0))
       log_out
     end
   end
