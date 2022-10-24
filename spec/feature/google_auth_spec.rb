0526d9b90dd891e5e71a56bdd6901c65cb0c0893
MengtingCao <mengting.cao@tamu.edu>
2022-10-24 16:02:44 -0500

    moved mailer test to own spec

--- spec/feature/google_auth_spec.rb
+++ spec/feature/google_auth_spec.rb
@@ -71,17 +71,6 @@ RSpec.describe('google auth', type: :feature) do
       click_on 'Update'
       expect(page).to(have_content('Your account has been updated successfully.'))
     end
-
-    it 'sends password reset email' do
-      log_in_member
-      member_email = UserHelpers.class_variable_get(:@@member_email)
-      log_out
-      visit new_user_session_path
-      find('#Forgot_Password').click
-      fill_in 'Email', with: member_email
-      click_on 'Send me reset password instructions'
-      expect(Devise.mailer.deliveries.count).to(eq(1))
-    end
   end
 
   describe 'mock account with existing email' do
