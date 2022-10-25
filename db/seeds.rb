41ab53654db1c0d160d2007ca2b1db0cceb3be4a
Nicholas Roth <nroth020@gmail.com>
2022-10-25 18:01:13 -0500

    for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create([
-  {
-    email: 'jswood@tamu.edu',
-    full_name: 'Josh test admin',
-    admin: true
-  }
-]
-           )
-
 Page.create([
   {
     page_name: 'about',
