8a16ef2bd7ee2e2df69edae9000022de7178aac2
Nicholas Roth <nroth020@gmail.com>
2022-10-25 09:14:45 -0500

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 2022_10_17_024124) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "pages", force: :cascade do |t|
+    t.integer "pid"
+    t.integer "auto_increment"
+    t.string "page_name"
+    t.json "page_data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
