0f5488c345b299869ea93d0f76336bfe1d771f0d
julianaLeano <julexd125@gmail.com>
2022-10-26 21:13:23 -0500

    added basic contact info and integration tests

---
+++ spec/views/internal/internal_contact.html.erb_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require './spec/support/user_helpers'
+
+RSpec.describe('internal/internal_contact.html.erb', type: :feature) do
+  it 'shows header' do
+    log_in_member
+    visit 'internal_contact'
+    expect(page).to(have_content('Members Contact Information'))
+  end
+end
