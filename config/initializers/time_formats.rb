ae1e960c992f225ebdc7c99f4a3b2ad6352768cf
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-11-08 15:02:42 -0600

    changed basic layout and date format of event card

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -6,3 +6,10 @@ Time::DATE_FORMATS[:get_date] = lambda { |time| time.strftime("%m-%d-%Y")}
 
 # 05:21 PM
 Time::DATE_FORMATS[:get_time] = lambda { |time| time.strftime("%I:%M %p")}
+
+# NOV
+Time::DATE_FORMATS[:get_month] = lambda { |time| time.strftime("%b").upcase!}
+# NOV
+Time::DATE_FORMATS[:get_day] = lambda { |time| time.strftime("%d")}
+# NOV
+Time::DATE_FORMATS[:get_year] = lambda { |time| time.strftime("%Y")}
