e50192e1e5f4162b80498229af7da5c5e6bf6d4e
Yuning Zhang <yuning.zhang09@gmail.com>
2022-11-10 18:45:39 -0600

    Fixed formatting issues with rubocop

--- spec/views/external/about.html.erb_spec.rb
+++ spec/views/external/about.html.erb_spec.rb
@@ -2,36 +2,33 @@
 
 require 'rails_helper'
 require './spec/support/entity_helpers'
-require 'pp'
-
 
 RSpec.describe('external/about.html.erb', type: :feature) do
   before do
     visit '/about'
   end
 
-  it 'should show main header' do
+  it 'shows main header' do
     expect(page).to(have_content('About Us'))
   end
 
-  it 'should show correct subheaders' do
-    about_page = Page.where(page_name: "about").pluck(:page_data)
+  it 'shows correct subheaders' do
+    about_page = Page.where(page_name: 'about').pluck(:page_data)
     about_page.each do |attr|
-        json = JSON.parse(attr)
-        json.each do |name, desc|
-            expect(page).to(have_content(name))
-        end
+      json = JSON.parse(attr)
+      json.each do |name, _desc|
+        expect(page).to(have_content(name))
+      end
     end
   end
 
-  it 'should show correct descriptions' do
-    about_page = Page.where(page_name: "about").pluck(:page_data)
+  it 'shows correct descriptions' do
+    about_page = Page.where(page_name: 'about').pluck(:page_data)
     about_page.each do |attr|
-        json = JSON.parse(attr)
-        json.each do |name, desc|
-            expect(page).to(have_content(desc))
-        end
+      json = JSON.parse(attr)
+      json.each do |_name, desc|
+        expect(page).to(have_content(desc))
+      end
     end
   end
-
 end
