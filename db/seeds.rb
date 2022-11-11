b554ced49cf9ecbbcfa83ceccca54a201eaf6a5e
Yuning Zhang <yuning.zhang09@gmail.com>
2022-11-11 15:10:49 -0600

    Updated initial db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,29 +13,29 @@ if Page.count < 3
     {
       page_name: 'about',
       page_data: '{
-        "purpose" : "The purpose of this organization shall be to advocate for maternal and child public health-related issues for all students.",
+        "Purpose" : "The purpose of this organization shall be to advocate for maternal and child public health-related issues for all students.",
 
-        "goal" : "The goal will be to enhance the quality and scope of maternal and child public health by educating students and providing them with opportunities to impact the community positively.",
+        "Goal" : "The goal will be to enhance the quality and scope of maternal and child public health by educating students and providing them with opportunities to impact the community positively.",
 
-        "membership_dues" : "Membership is open to any Aggie (undergraduate or graduate/ any concentration). Dues are $15 one time.",
+        "Membership Dues" : "Membership is open to any Aggie (undergraduate or graduate/ any concentration). Dues are $15 one time.",
 
-        "expectations" : [
-          "Attend monthly meetings",
-          "Respond in GroupMe when needed (ex. polls)",
-          "Attend supplemental meetings if able",
-          "Respect opinions of fellow members"],
+        "Expectations" : 
+          "Attend monthly meetings,
+          Respond in GroupMe when needed (ex. polls),
+          Attend supplemental meetings if able,
+          Respect opinions of fellow members",
 
-          "what_we_do" : [
-          "Field Trips",
-          "Volunteering",
-          "Book Club",
-          "Guest Speakers",
-          "Donation Drives",
-          "Profit Shares",
-          "Craft Events",
-          "Socials",
-          "Research Opportunities",
-          "Certificate Opportunities"]
+          "What We Do" : 
+          "Field Trips,
+          Volunteering,
+          Book Club,
+          Guest Speakers,
+          Donation Drives,
+          Profit Shares,
+          Craft Events,
+          Socials,
+          Research Opportunities,
+          Certificate Opportunities"
   }'
     },
     {
