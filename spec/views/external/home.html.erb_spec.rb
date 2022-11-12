23d50432b9d1eff9ee3bd7e8289cdeed13b7ad41
julianaLeano <julexd125@gmail.com>
2022-11-11 22:00:50 -0600

    home page design, added world cloud asset, and included container update from pr

--- spec/views/external/home.html.erb_spec.rb
+++ spec/views/external/home.html.erb_spec.rb
@@ -9,6 +9,6 @@ RSpec.describe('external/home.html.erb', type: :feature) do
   end
 
   it 'shows introduction content' do
-    expect(page).to(have_content('Welcome to the Maternal and Child Health Student Organization'))
+    expect(page).to(have_content('The Maternal and Child Health Student Organization'))
   end
 end
