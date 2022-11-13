0a42530d887d1021f98606ad18517193a2d90625
Yuning Zhang <yuning.zhang09@gmail.com>
2022-11-13 01:12:29 -0600

    fixed rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # if Page.count < 3
-  Page.create([
-    {
-      page_name: 'about',
-      page_data: '{
+Page.create([
+  {
+    page_name: 'about',
+    page_data: '{
         "Purpose" : "The purpose of this organization shall be to advocate for maternal and child public health-related issues for all students.",
 
         "Goal" : "The goal will be to enhance the quality and scope of maternal and child public health by educating students and providing them with opportunities to impact the community positively.",
@@ -23,10 +23,10 @@
 
         "What We Do" : "Field Trips, Volunteering, Book Club, Guest Speakers, Donation Drives, Profit Shares, Craft Events, Socials, Research Opportunities, Certificate Opportunities"
   }'
-    },
-    {
-      page_name: 'contact',
-      page_data: '{
+  },
+  {
+    page_name: 'contact',
+    page_data: '{
         "description" : "Below you can find our email and social links, be sure to follow MCHSO on Facebook!",
 
         "Email" : "mailto:hannahbartosh@tamu.edu",
@@ -37,17 +37,17 @@
           "instagram" : "https://www.instagram.com/mchsoaggies/?hl=en"
         }
   }'
-    },
-    {
-      page_name: 'home',
-      page_data: '{
+  },
+  {
+    page_name: 'home',
+    page_data: '{
         "title" : "Welcome to the Maternal and Child Health Student Organization",
 
         "blurb" : "Welcome blurb, summary of the history and basic information of the org."
   }'
-    }
-  ]
-             )
+  }
+]
+           )
 # end
 
 if PointsType.all.count.zero?
