6982ac51fb7055c7481640175e68ea56c0e1d00f
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-11-16 12:28:25 -0600

    delete relevant attendance records after deleting an event

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,8 @@ class Event < ApplicationRecord
   validates :passcode, presence: true
   validates :date, presence: true
   validates :points, presence: true
+
+  after_destroy do |this_event|
+    AttendanceRecord.where(event_id: this_event.id).delete_all
+  end
 end
